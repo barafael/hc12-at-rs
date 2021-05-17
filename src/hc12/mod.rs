829107480d33f1fefafdc2ee1681cfc979036f0f
Rafael Bachmann <mediumendian@gmail.com>
2021-05-17 23:15:37 +0200

    Clear up fallible state transitions with newtypes

--- src/hc12/mod.rs
+++ src/hc12/mod.rs
@@ -1,3 +1,5 @@
+//! Type-state driven state machine of Hc12.
+
 use core::marker::PhantomData;
 
 use embedded_hal::blocking::delay::DelayMs;
@@ -8,8 +10,8 @@ use embedded_hal::{
 use nb::*;
 
 use crate::config::parameters::{
-    Parameters, OK_QUERY, OK_RESPONSE, RESET_SETTINGS_COMMAND, RESET_SETTINGS_RESPONSE,
-    SLEEP_COMMAND, SLEEP_RESPONSE, VERSION_QUERY,
+    OK_QUERY, OK_RESPONSE, RESET_SETTINGS_COMMAND, RESET_SETTINGS_RESPONSE, SLEEP_COMMAND,
+    SLEEP_RESPONSE, VERSION_QUERY,
 };
 
 #[cfg(test)]
@@ -24,14 +26,25 @@ pub struct Configuration;
 /// Sleep mode marker
 pub struct Sleep;
 
+/// Fallible transition from normal to config state
+type NormalToConfig<S, P, D> =
+    core::result::Result<Hc12<S, P, D, Configuration>, Hc12<S, P, D, Normal>>;
+
+/// Fallible transition from config to normal state
+type ConfigToNormal<S, P, D> =
+    core::result::Result<Hc12<S, P, D, Normal>, Hc12<S, P, D, Configuration>>;
+
+/// Fallible transition from config to sleep state
 type ConfigToSleep<S, P, D> =
     core::result::Result<Hc12<S, P, D, Sleep>, Hc12<S, P, D, Configuration>>;
 
+/// Fallible transition from sleep to config state
 type SleepToConfig<S, P, D> =
     core::result::Result<Hc12<S, P, D, Configuration>, Hc12<S, P, D, Sleep>>;
 
+/// Hc12 resources: A serial port, an output pin, and a Delay.
 #[derive(Debug)]
-pub struct Hc12<S, P, D, T>
+pub struct Hc12<S, P, D, M>
 where
     S: Read<u8> + Write<u8>,
     P: OutputPin,
@@ -40,16 +53,17 @@ where
     serial: S,
     set_pin: P,
     delay: D,
-    parameters: Parameters,
-    mode: PhantomData<T>,
+    mode: PhantomData<M>,
 }
 
+/// Implementation for normal mode of Hc12
 impl<S, P, D> Hc12<S, P, D, Normal>
 where
     S: Read<u8> + Write<u8>,
     P: OutputPin,
     D: DelayMs<u32>,
 {
+    /// Construct an Hc12 in normal mode
     pub fn new(serial: S, mut set_pin: P, mut delay: D) -> Self {
         let _ = set_pin.set_high();
         delay.delay_ms(20); // TODO which duration?
@@ -59,15 +73,16 @@ where
             serial,
             set_pin,
             delay,
-            parameters: Parameters::default(),
             mode: PhantomData::<Normal>,
         }
     }
 
+    /// Release the contained resources
     pub fn release(self) -> (S, P, D) {
         (self.serial, self.set_pin, self.delay)
     }
 
+    /// Write entire buffer to serial port
     pub fn write_buffer(&mut self, buffer: &[u8]) -> Result<(), Error<crate::Error>> {
         for ch in buffer.iter() {
             let _ = block!(self.serial.write(*ch));
@@ -75,6 +90,7 @@ where
         Ok(())
     }
 
+    /// Read entire buffer from serial port
     pub fn read_buffer(&mut self, buffer: &mut [u8]) -> Result<(), Error<crate::Error>> {
         let mut n = 0;
         while n < buffer.len() {
@@ -86,20 +102,21 @@ where
         Ok(())
     }
 
-    pub fn into_configuration_mode(mut self) -> Hc12<S, P, D, Configuration> {
+    /// Move Hc12 in normal mode to Hc12 in config mode
+    pub fn into_configuration_mode(mut self) -> NormalToConfig<S, P, D> {
         let _ = self.set_pin.set_low();
         self.delay.delay_ms(40); // TODO how long?
-        Hc12 {
+        Ok(Hc12 {
             serial: self.serial,
             set_pin: self.set_pin,
-            parameters: self.parameters,
             delay: self.delay,
             mode: PhantomData::<Configuration>,
-        }
+        })
     }
 }
 
-// TODO use T instead of u8
+/// Implement Read for Hc12 in normal mode
+/// This just defers to the underlying serial implementation.
 impl<S, P, D> embedded_hal::serial::Read<u8> for Hc12<S, P, D, Normal>
 where
     S: Read<u8> + Write<u8>,
@@ -113,6 +130,8 @@ where
     }
 }
 
+/// Implement Write for Hc12 in normal mode.
+/// This just defers to the underlying serial implementation.
 impl<S, P, D> embedded_hal::serial::Write<u8> for Hc12<S, P, D, Normal>
 where
     S: embedded_hal::serial::Read<u8> + embedded_hal::serial::Write<u8>,
@@ -129,24 +148,26 @@ where
     }
 }
 
+/// Implementation for Hc12 in config mode.
 impl<S, P, D> Hc12<S, P, D, Configuration>
 where
     S: Read<u8> + Write<u8>,
     P: OutputPin,
     D: DelayMs<u32>,
 {
-    pub fn into_normal_mode(mut self) -> Hc12<S, P, D, Normal> {
+    /// Move Hc12 in config mode back to normal mode.
+    pub fn into_normal_mode(mut self) -> ConfigToNormal<S, P, D> {
         let _ = self.set_pin.set_high();
         self.delay.delay_ms(40); // TODO how long?
-        Hc12 {
+        Ok(Hc12 {
             serial: self.serial,
             set_pin: self.set_pin,
-            parameters: self.parameters,
             delay: self.delay,
             mode: PhantomData::<Normal>,
-        }
+        })
     }
 
+    /// Move Hc12 in config mode into sleeping mode.
     pub fn into_sleeping_mode(mut self) -> ConfigToSleep<S, P, D> {
         for ch in SLEEP_COMMAND.iter() {
             let _ = block!(self.serial.write(*ch));
@@ -167,7 +188,6 @@ where
             Ok(Hc12 {
                 delay: self.delay,
                 mode: PhantomData::<Sleep>,
-                parameters: self.parameters,
                 set_pin: self.set_pin,
                 serial: self.serial,
             })
@@ -176,6 +196,7 @@ where
         }
     }
 
+    /// Check if Hc12 responds to "AT" query with "OK".
     pub fn is_ok(&mut self) -> bool {
         for ch in OK_QUERY.iter() {
             let _ = block!(self.serial.write(*ch));
@@ -191,6 +212,7 @@ where
         buffer == OK_RESPONSE
     }
 
+    /// Get version as a string from Hc12
     pub fn get_version<'a>(&mut self, buffer: &'a mut [u8; 16]) -> &'a [u8] {
         for ch in VERSION_QUERY.iter() {
             let _ = block!(self.serial.write(*ch));
@@ -208,6 +230,7 @@ where
         &buffer[..count]
     }
 
+    /// Reset Hc12 to default settings.
     pub fn reset_settings(&mut self) -> bool {
         for ch in RESET_SETTINGS_COMMAND.iter() {
             let _ = block!(self.serial.write(*ch));
@@ -228,19 +251,20 @@ where
     }
 }
 
+/// Implementation for Hc12 in sleeping mode.
 impl<S, P, D> Hc12<S, P, D, Sleep>
 where
     S: Read<u8> + Write<u8>,
     P: OutputPin,
     D: DelayMs<u32>,
 {
+    /// Move Hc12 in sleeping mode back to configuration mode.
     pub fn into_configuration_mode(mut self) -> SleepToConfig<S, P, D> {
         let _ = self.set_pin.set_low();
         self.delay.delay_ms(40); // TODO how long?
         Ok(Hc12 {
             serial: self.serial,
             set_pin: self.set_pin,
-            parameters: self.parameters,
             delay: self.delay,
             mode: PhantomData::<Configuration>,
         })
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,26 @@
+//! Hc12 driver
+//! This driver implements normal, config and sleep functionality of the hc12 module.
+
 #![cfg_attr(not(test), no_std)]
 
+#![deny(unsafe_code)]
+#![deny(missing_docs)]
+
+/// Configuration data structures
 mod config;
 
+/// Hc12 driver
 pub mod hc12;
 
+/// Crate error
 #[derive(Debug)]
 pub enum Error {
+    /// Read error
     Read,
+    /// Write error
     Write,
+    /// Invalid channel
     InvalidChannel(u8),
+    /// Invalid baud rate
     InvalidBaudRate,
 }
