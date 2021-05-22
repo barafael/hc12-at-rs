8ed3e4947e70f92d9777a1a4cda3076b7e5bd98e
Rafael Bachmann <mediumendian@gmail.com>
2021-05-22 23:13:40 +0200

    Add usage example to tests

--- src/hc12/test.rs
+++ src/hc12/test.rs
@@ -1,23 +1,23 @@
 use super::*;
 
 use debugless_unwrap::DebuglessUnwrap;
-use embedded_hal_mock::delay::MockNoop;
 use embedded_hal_mock::pin::*;
+use embedded_hal_mock::{delay::MockNoop, pin, serial};
 
 #[test]
 fn is_ok() {
     let delay = MockNoop;
     let pin_transactions = [
-        embedded_hal_mock::pin::Transaction::set(State::High),
-        embedded_hal_mock::pin::Transaction::set(State::Low),
-        embedded_hal_mock::pin::Transaction::set(State::High),
+        pin::Transaction::set(State::High),
+        pin::Transaction::set(State::Low),
+        pin::Transaction::set(State::High),
     ];
-    let set_pin = embedded_hal_mock::pin::Mock::new(&pin_transactions);
+    let set_pin = pin::Mock::new(&pin_transactions);
     let transactions = [
-        embedded_hal_mock::serial::Transaction::write_many(b"AT\r\n"),
-        embedded_hal_mock::serial::Transaction::read_many(b"OK\r\n"),
+        serial::Transaction::write_many(b"AT\r\n"),
+        serial::Transaction::read_many(b"OK\r\n"),
     ];
-    let serial = embedded_hal_mock::serial::Mock::new(&transactions);
+    let serial = serial::Mock::new(&transactions);
     let hc12 = Hc12::new(serial, set_pin, delay);
     let mut hc12 = hc12.into_configuration_mode().debugless_unwrap();
     let ok = hc12.is_ok();
@@ -31,11 +31,9 @@ fn is_ok() {
 #[test]
 fn send_buffer() {
     let delay = MockNoop;
-    let set_pin = embedded_hal_mock::pin::Mock::new(&[Transaction::set(State::High)]);
-    let transactions = [
-        embedded_hal_mock::serial::Transaction::write_many(b"some data AT AT\r\n"),
-    ];
-    let serial = embedded_hal_mock::serial::Mock::new(&transactions);
+    let set_pin = pin::Mock::new(&[pin::Transaction::set(State::High)]);
+    let transactions = [serial::Transaction::write_many(b"some data AT AT\r\n")];
+    let serial = serial::Mock::new(&transactions);
     let mut hc12 = Hc12::new(serial, set_pin, delay);
     hc12.write_buffer(b"some data AT AT\r\n").unwrap();
     let (mut serial, mut set_pin, _) = hc12.release();
@@ -46,11 +44,9 @@ fn send_buffer() {
 #[test]
 fn receive_buffer() {
     let delay = MockNoop;
-    let set_pin = embedded_hal_mock::pin::Mock::new(&[Transaction::set(State::High)]);
-    let transactions = [
-        embedded_hal_mock::serial::Transaction::read_many(b"some data AT AT\r\n"),
-    ];
-    let serial = embedded_hal_mock::serial::Mock::new(&transactions);
+    let set_pin = pin::Mock::new(&[pin::Transaction::set(State::High)]);
+    let transactions = [serial::Transaction::read_many(b"some data AT AT\r\n")];
+    let serial = serial::Mock::new(&transactions);
     let mut hc12 = Hc12::new(serial, set_pin, delay);
     let mut buffer = [0u8; 32];
     hc12.read_buffer(&mut buffer[..17]).unwrap();
@@ -61,19 +57,68 @@ fn receive_buffer() {
 }
 
 #[test]
+fn usage_from_readme() {
+    let delay = MockNoop;
+    let set_pin = pin::Mock::new(&[
+        pin::Transaction::set(State::High),
+        pin::Transaction::set(State::Low),
+        pin::Transaction::set(State::High),
+    ]);
+    let transactions = [
+        serial::Transaction::write_many(b"AT\r\n"),
+        serial::Transaction::read_many(b"OK\r\n"),
+        serial::Transaction::write_many(b"AT+V\r\n"),
+        serial::Transaction::read_many(b"VERSION-42\r\n"),
+        serial::Transaction::write_many(b"AT+RX\r\n"),
+        serial::Transaction::read_many(b"OK+FU2\r\nOK+B115200\r\nOK+RC042\r\nOK+RP:-1dBm\r\n"),
+        serial::Transaction::write_many(b"some data AT AT\r\n"),
+    ];
+    let serial = serial::Mock::new(&transactions);
+    let hc12 = Hc12::new(serial, set_pin, delay);
+    let mut hc12 = hc12.into_configuration_mode().debugless_unwrap();
+
+    let mut buffer = [0u8; 16];
+
+    let ok = hc12.is_ok();
+    assert!(ok);
+
+    let version = hc12.get_version(&mut buffer);
+    assert_eq!(version, b"VERSION-42\r\n");
+
+    let params = hc12.get_parameters().unwrap();
+    assert_eq!(
+        Parameters {
+            baud_rate: BaudRate::Bps115200,
+            channel: Channel(42),
+            power: TransmissionPower(1),
+            mode: Mode::Fu2,
+        },
+        params
+    );
+
+    let mut hc12 = hc12.into_normal_mode().debugless_unwrap();
+
+    hc12.write_buffer(b"some data AT AT\r\n").unwrap();
+
+    let (mut serial, mut set_pin, _) = hc12.release();
+    serial.done();
+    set_pin.done();
+}
+
+#[test]
 fn get_version() {
     let delay = MockNoop;
     let pin_transactions = [
-        embedded_hal_mock::pin::Transaction::set(State::High),
-        embedded_hal_mock::pin::Transaction::set(State::Low),
-        embedded_hal_mock::pin::Transaction::set(State::High),
+        pin::Transaction::set(State::High),
+        pin::Transaction::set(State::Low),
+        pin::Transaction::set(State::High),
     ];
-    let set_pin = embedded_hal_mock::pin::Mock::new(&pin_transactions);
+    let set_pin = pin::Mock::new(&pin_transactions);
     let transactions = [
-        embedded_hal_mock::serial::Transaction::write_many(b"AT+V\r\n"),
-        embedded_hal_mock::serial::Transaction::read_many(b"HC-12_VFAKE\r\n"),
+        serial::Transaction::write_many(b"AT+V\r\n"),
+        serial::Transaction::read_many(b"HC-12_VFAKE\r\n"),
     ];
-    let serial = embedded_hal_mock::serial::Mock::new(&transactions);
+    let serial = serial::Mock::new(&transactions);
     let hc12 = Hc12::new(serial, set_pin, delay);
     let mut hc12 = hc12.into_configuration_mode().debugless_unwrap();
     let mut buffer = [0u8; 16];
@@ -89,18 +134,16 @@ fn get_version() {
 fn get_parameters() {
     let delay = MockNoop;
     let pin_transactions = [
-        embedded_hal_mock::pin::Transaction::set(State::High),
-        embedded_hal_mock::pin::Transaction::set(State::Low),
-        embedded_hal_mock::pin::Transaction::set(State::High),
+        pin::Transaction::set(State::High),
+        pin::Transaction::set(State::Low),
+        pin::Transaction::set(State::High),
     ];
-    let set_pin = embedded_hal_mock::pin::Mock::new(&pin_transactions);
+    let set_pin = pin::Mock::new(&pin_transactions);
     let transactions = [
-        embedded_hal_mock::serial::Transaction::write_many(b"AT+RX\r\n"),
-        embedded_hal_mock::serial::Transaction::read_many(
-            b"OK+FU3\r\nOK+B9600\r\nOK+RC001\r\nOK+RP:+20dBm\r\n",
-        ),
+        serial::Transaction::write_many(b"AT+RX\r\n"),
+        serial::Transaction::read_many(b"OK+FU3\r\nOK+B9600\r\nOK+RC001\r\nOK+RP:+20dBm\r\n"),
     ];
-    let serial = embedded_hal_mock::serial::Mock::new(&transactions);
+    let serial = serial::Mock::new(&transactions);
     let hc12 = Hc12::new(serial, set_pin, delay);
     let mut hc12 = hc12.into_configuration_mode().debugless_unwrap();
     let params = hc12.get_parameters().unwrap();
@@ -121,18 +164,16 @@ fn get_parameters() {
 fn get_more_parameters() {
     let delay = MockNoop;
     let pin_transactions = [
-        embedded_hal_mock::pin::Transaction::set(State::High),
-        embedded_hal_mock::pin::Transaction::set(State::Low),
-        embedded_hal_mock::pin::Transaction::set(State::High),
+        pin::Transaction::set(State::High),
+        pin::Transaction::set(State::Low),
+        pin::Transaction::set(State::High),
     ];
-    let set_pin = embedded_hal_mock::pin::Mock::new(&pin_transactions);
+    let set_pin = pin::Mock::new(&pin_transactions);
     let transactions = [
-        embedded_hal_mock::serial::Transaction::write_many(b"AT+RX\r\n"),
-        embedded_hal_mock::serial::Transaction::read_many(
-            b"OK+FU1\r\nOK+B115200\r\nOK+RC101\r\nOK+RP:-1dBm\r\n",
-        ),
+        serial::Transaction::write_many(b"AT+RX\r\n"),
+        serial::Transaction::read_many(b"OK+FU1\r\nOK+B115200\r\nOK+RC101\r\nOK+RP:-1dBm\r\n"),
     ];
-    let serial = embedded_hal_mock::serial::Mock::new(&transactions);
+    let serial = serial::Mock::new(&transactions);
     let hc12 = Hc12::new(serial, set_pin, delay);
     let mut hc12 = hc12.into_configuration_mode().debugless_unwrap();
     let params = hc12.get_parameters().unwrap();
@@ -153,16 +194,16 @@ fn get_more_parameters() {
 fn reset_to_default() {
     let delay = MockNoop;
     let pin_transactions = [
-        embedded_hal_mock::pin::Transaction::set(State::High),
-        embedded_hal_mock::pin::Transaction::set(State::Low),
-        embedded_hal_mock::pin::Transaction::set(State::High),
+        pin::Transaction::set(State::High),
+        pin::Transaction::set(State::Low),
+        pin::Transaction::set(State::High),
     ];
-    let set_pin = embedded_hal_mock::pin::Mock::new(&pin_transactions);
+    let set_pin = pin::Mock::new(&pin_transactions);
     let transactions = [
-        embedded_hal_mock::serial::Transaction::write_many(b"AT+DEFAULT\r\n"),
-        embedded_hal_mock::serial::Transaction::read_many(b"OK+DEFAULT\r\n"),
+        serial::Transaction::write_many(b"AT+DEFAULT\r\n"),
+        serial::Transaction::read_many(b"OK+DEFAULT\r\n"),
     ];
-    let serial = embedded_hal_mock::serial::Mock::new(&transactions);
+    let serial = serial::Mock::new(&transactions);
     let hc12 = Hc12::new(serial, set_pin, delay);
     let mut hc12 = hc12.into_configuration_mode().debugless_unwrap();
     let result = hc12.reset_settings();
@@ -177,18 +218,18 @@ fn reset_to_default() {
 fn go_to_sleep_and_wake_up() {
     let delay = MockNoop;
     let pin_transactions = [
-        embedded_hal_mock::pin::Transaction::set(State::High),
-        embedded_hal_mock::pin::Transaction::set(State::Low),
-        embedded_hal_mock::pin::Transaction::set(State::High),
-        embedded_hal_mock::pin::Transaction::set(State::Low),
-        embedded_hal_mock::pin::Transaction::set(State::High),
+        pin::Transaction::set(State::High),
+        pin::Transaction::set(State::Low),
+        pin::Transaction::set(State::High),
+        pin::Transaction::set(State::Low),
+        pin::Transaction::set(State::High),
     ];
-    let set_pin = embedded_hal_mock::pin::Mock::new(&pin_transactions);
+    let set_pin = pin::Mock::new(&pin_transactions);
     let transactions = [
-        embedded_hal_mock::serial::Transaction::write_many(b"AT+SLEEP\r\n"),
-        embedded_hal_mock::serial::Transaction::read_many(b"OK+SLEEP\r\n"),
+        serial::Transaction::write_many(b"AT+SLEEP\r\n"),
+        serial::Transaction::read_many(b"OK+SLEEP\r\n"),
     ];
-    let serial = embedded_hal_mock::serial::Mock::new(&transactions);
+    let serial = serial::Mock::new(&transactions);
     let hc12 = Hc12::new(serial, set_pin, delay);
     let hc12 = hc12.into_configuration_mode().debugless_unwrap();
     let result = hc12.into_sleeping_mode();
